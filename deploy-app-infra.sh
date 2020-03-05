2e8676342c50ae238108063177dbf66d2c72f3ac
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-03-06 00:50:54 +1100

    Added missing init step

--- deploy-app-infra.sh
+++ deploy-app-infra.sh
@@ -4,4 +4,8 @@ readonly lambda_package=app.zip
 
 (cd webapp && zip -q -r9 "../$lambda_package" *)
 
-(cd app-infra && terraform apply -auto-approve -var="lambda_package_file=../$lambda_package")
+(
+    cd app-infra \
+    && terraform init -no-color \
+    && terraform apply -auto-approve -var="lambda_package_file=../$lambda_package" -no-color
+)
